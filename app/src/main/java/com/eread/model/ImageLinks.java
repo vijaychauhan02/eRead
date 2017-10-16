09cb2ede1cfcc4dac2cf764a3b084c14702c4ea0
Vijay Chauhan <cvijay@thoughtworks.com>
2017-10-16 13:36:53 +0530

    adding Picasso as a image network library and adding fetching images of search result books

--- app/src/main/java/com/eread/model/ImageLinks.java
+++ app/src/main/java/com/eread/model/ImageLinks.java
@@ -1,6 +1,19 @@
 package com.eread.model;
 
-class ImageLinks {
+import android.databinding.BindingAdapter;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class ImageLinks {
   public String smallThumbnail;
   public String thumbnail;
+
+  @BindingAdapter({"bind:imageUrl"})
+  public static void loadImage(ImageView view, String imageUrl) {
+    Picasso.with(view.getContext())
+        .load(imageUrl)
+        .placeholder(android.R.color.transparent)
+        .into(view);
+  }
 }
